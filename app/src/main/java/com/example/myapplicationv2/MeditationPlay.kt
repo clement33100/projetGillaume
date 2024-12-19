eb1bb8c9fbbb250babc08bb77d224008d3a06d86
HugoFougeret <hugo_fougeret@orange.fr>
2024-12-19 10:21:26 +0100

    page meditationplay

--- app/src/main/java/com/example/myapplicationv2/MeditationPlay.kt
+++ app/src/main/java/com/example/myapplicationv2/MeditationPlay.kt
@@ -7,10 +7,8 @@ import android.os.Handler
 import android.os.Looper
 import android.speech.tts.TextToSpeech
 import android.util.Log
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ProgressBar
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -34,10 +32,7 @@ class MeditationPlay : AppCompatActivity() {
     private lateinit var progressBar: ProgressBar
     private var handler: Handler? = null
     private var handlerAffirmation: Handler? = null
-
-    private lateinit var buttonPause: ImageButton
-    private var isPlaying = true // Variable pour suivre l'état du lecteur
-
+    private var isPaused = false
     private var currentIndex = 0 // Ajout d'un index pour suivre l'élément actuel de userTexts
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,9 +53,6 @@ class MeditationPlay : AppCompatActivity() {
         val savedFilePathBolTibetain = copyRawResourceToInternalStorage(R.raw.boltibetainson, boltibetain)
 
 
-        buttonPause = findViewById<ImageButton>(R.id.imageButtonPause)
-
-
 
 
         val filePaths = intent.getStringExtra("filePaths")
@@ -76,34 +68,6 @@ class MeditationPlay : AppCompatActivity() {
             Log.d("test1234888", "No texts received")
         }
 
-        buttonPause.setOnClickListener {
-            if (isPlaying) {
-                // Mettre en pause les lecteurs audio
-                mediaPlayer?.pause()
-                preAudioPlayer?.pause()
-                afterAudioPlayer?.pause()
-                affirmationAudioPlayer?.pause()
-                EndAudioPlayer?.pause()
-                // Supprime les callbacks en pause
-                handlerAffirmation?.removeCallbacksAndMessages(null)
-
-                // Change l'icône en "play"
-                buttonPause.setImageResource(R.drawable.play)
-            } else {
-                // Reprendre les lecteurs audio
-                mediaPlayer?.start()
-                preAudioPlayer?.start()
-                afterAudioPlayer?.start()
-                affirmationAudioPlayer?.start()
-
-                // Relancer les affirmations
-
-
-                // Change l'icône en "pause"
-                buttonPause.setImageResource(R.drawable.pause)
-            }
-            isPlaying = !isPlaying // Basculer l'état
-        }
 
 
 
@@ -121,13 +85,33 @@ class MeditationPlay : AppCompatActivity() {
             playMainAudio(filePaths,savedFilePathBolTibetain,selectedDurationInSeconds)
 
         }
+        val pauseButton = findViewById<ImageButton>(R.id.imageButtonPause)
 
+        pauseButton.setOnClickListener {
+            if (isPaused) {
+                // Relancer la lecture de tous les players
+                mediaPlayer?.start()
+                affirmationAudioPlayer?.start()
+                afterAudioPlayer?.start()
+                preAudioPlayer?.start()
+                EndAudioPlayer?.start()
+                TTSAudioPlayer?.start()
 
-
-
+                pauseButton.setImageResource(R.drawable.imgunpause)
+                isPaused = false
+            } else {
+                // Mettre en pause tous les players
+                mediaPlayer?.pause()
+                affirmationAudioPlayer?.pause()
+                afterAudioPlayer?.pause()
+                preAudioPlayer?.pause()
+                EndAudioPlayer?.pause()
+                TTSAudioPlayer?.pause()
+                pauseButton.setImageResource(R.drawable.imgpause)
+                isPaused = true
+            }
+        }
     }
-
-
 
 
     private fun copyRawResourceToInternalStorage(resourceId: Int, fileName: String): String? {
@@ -421,9 +405,6 @@ class MeditationPlay : AppCompatActivity() {
         handlerAffirmation?.removeCallbacksAndMessages(null) // Remove any pending callbacks
         handlerAffirmation = null
         handler?.removeCallbacksAndMessages(null) // Remove any pending callbacks
-        isPlaying = false
-        buttonPause.setImageResource(R.drawable.pause) // Réinitialise à l'icône de lecture
-
         handler = null
     }
 
@@ -474,7 +455,6 @@ class MeditationPlay : AppCompatActivity() {
             }
         })
     }
-
 
 
 
