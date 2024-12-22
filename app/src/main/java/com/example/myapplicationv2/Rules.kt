da62900ea24672f615385e82a55ae124bf7e7d09
clement33100 <clement.fougeret@etu.unilim.fr>
2024-12-22 12:08:35 +0100

    add link / rules

--- app/src/main/java/com/example/myapplicationv2/Rules.kt
+++ app/src/main/java/com/example/myapplicationv2/Rules.kt
@@ -11,35 +11,22 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Rules : AppCompatActivity() {
-    @SuppressLint("WrongViewCast")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_rules)
 
-        // Référencer l'ImageView par son ID
-        val btnImgRulesRight = findViewById<ImageView>(R.id.btnImg_rulesright)
 
-        // Définir un OnClickListener pour ouvrir l'activité Step2
-        btnImgRulesRight.setOnClickListener {
-            val intent = Intent(this, Step2::class.java)
-            startActivity(intent)
-        }
         // Référencer l'ImageView par son ID
         val btnStart = findViewById<Button>(R.id.btn_start)
 
         // Définir un OnClickListener pour ouvrir l'activité Step2
         btnStart.setOnClickListener {
-            val intent = Intent(this, Step2::class.java)
+            val intent = Intent(this, step1FirstName::class.java)
             startActivity(intent)
         }
 
 
-        // Gérer les insets pour l'UI
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
--- app/src/main/java/com/example/myapplicationv2/mesAffirmations.kt
+++ app/src/main/java/com/example/myapplicationv2/mesAffirmations.kt
@@ -3,6 +3,7 @@ package com.example.myapplicationv2
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -13,12 +14,33 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class mesAffirmations :  AppCompatActivity() {  // Hérite de Base au lieu de AppCompatActivity
+    private lateinit var btnNew: Button
+    private lateinit var btnMyaffirmations: Button
+    private lateinit var btnHowItWorks: Button
+    private lateinit var btnAdvices: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_mesaffirmations) // Utilisez le layout fourni
+        btnNew = findViewById<Button>(R.id.btn_new)
+        btnMyaffirmations = findViewById<Button>(R.id.btn_affirmation)
+        btnHowItWorks = findViewById<Button>(R.id.btn_howitworks)
+        btnAdvices = findViewById<Button>(R.id.btn_advicesaffirmations)
+
+
+        btnNew.setOnClickListener{
+
+            // Créez une intention pour lancer Stage2
+            val intent = Intent(this, Rules::class.java)
+            startActivity(intent)
+        }
+
+
     }
+
+
 
 
 
