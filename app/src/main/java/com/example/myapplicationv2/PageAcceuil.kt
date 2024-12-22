821ee528e2698099d94ccc4c536f6270d62360dc
clement33100 <clement.fougeret@etu.unilim.fr>
2024-12-22 11:14:50 +0100

    add page d'acceuit / my affirmations splash

--- app/src/main/java/com/example/myapplicationv2/PageAcceuil.kt
+++ app/src/main/java/com/example/myapplicationv2/PageAcceuil.kt
@@ -1,20 +1,29 @@
 package com.example.myapplicationv2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+import kotlinx.coroutines.*
+
 class PageAcceuil : AppCompatActivity() {
+
+    private val splashTimeOut: Long = 2000 // 2 secondes
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_page_acceuil)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_page_acceuil) // Utilisez le layout fourni
+
+        // Utilisation des Coroutines pour gérer le délai
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(splashTimeOut)
+            // Démarrer l'activité suivante
+            val intent = Intent(this@PageAcceuil, mesAffirmations::class.java)
+            startActivity(intent)
+            finish() // Finir l'activité Splash pour qu'elle ne soit pas dans le back stack
         }
     }
 }
